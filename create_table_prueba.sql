49301303199f4df84ff7b7b36a352e0cc559ae7d
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-09-28 16:57:35 -0400

    Agregando insert al scripts del modulo de estudiantes

--- create_table_prueba.sql
+++ create_table_prueba.sql
@@ -20,7 +20,7 @@ ALTER TABLE public.prueba
 CREATE TABLE public.carrera
 (
   id integer NOT NULL DEFAULT nextval('carrera_id_seq'::regclass),
-  name character varying(100) NOT NULL,
+  nombre character varying(100) NOT NULL,
   tipo character varying(100) NOT NULL,
   id_facultad integer NOT NULL,
   created_date timestamp with time zone NOT NULL DEFAULT now(),
@@ -29,7 +29,7 @@ CREATE TABLE public.carrera
   CONSTRAINT id_facultad FOREIGN KEY (id_facultad)
       REFERENCES public.facultad (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT nombre_carrera UNIQUE (name)
+  CONSTRAINT nombre_carrera UNIQUE (nombre)
 )
 WITH (
   OIDS=FALSE
@@ -107,3 +107,71 @@ WITH (
 );
 ALTER TABLE public.facultad
   OWNER TO postgres;
+
+
+INSERT INTO public.estudiante(
+            cedula, nacionalidad, nombre, apellido, sexo, fecha_nacimiento, 
+            telefono1, telefono2, email, edo_procedencia, id_carrera)
+    VALUES ('13381615', 'Venezolano', 'Luis', 'Servita', 'Masculino', '1976-07-07', 
+            '02418140120', '04265413615', 'luisservita777@gmail.com', 'Carabobo', 3);
+
+INSERT INTO public.estudiante(
+            cedula, nacionalidad, nombre, apellido, sexo, fecha_nacimiento, 
+            telefono1, telefono2, email, edo_procedencia, id_carrera)
+    VALUES ('22422883', 'Venezolano', 'Wilkel', 'Giovanni', 'Masculino', '1995-05-24', 
+            '02453351406', '04127658802', 'wilkelgiovanni@gmail.com', 'Carabobo', 3);
+
+INSERT INTO public.carrera(
+            nombre, tipo, id_facultad)
+    VALUES ('Computación', 'Semestre', 2);
+
+INSERT INTO public.carrera(
+            nombre, tipo, id_facultad)
+    VALUES ('Física', 'Semestre', 2);
+
+INSERT INTO public.carrera(
+            nombre, tipo, id_facultad)
+    VALUES ('Química', 'Semestre', 2);
+
+INSERT INTO public.carrera(
+            nombre, tipo, id_facultad)
+    VALUES ('Matemática', 'Semestre', 2);
+
+INSERT INTO public.carrera(
+            nombre, tipo, id_facultad)
+    VALUES ('Biología', 'Semestre', 2);
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ciencias y Tecnología');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ingeniería');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ciencias Jurídicas y Políticas');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ciencias de la Salud');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ciencias Económicas y Sociales');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Ciencias de la Educación');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Odontología');
+
+INSERT INTO public.facultad(
+            nombre)
+    VALUES ('Dirección General de Postgrado');
+    
+    
+    

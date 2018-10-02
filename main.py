b6db49f2e1e805f75199d6265f87df8fbbec2e40
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-10-02 19:34:06 -0400

    Se agrego el cubo de dimensiones y se realizo la api que recibira el modulo de reportes

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from variables import *
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
 import mysql.connector
+
+# Resources
+from resources.StudentFaculty import Student, StudentMaleFaculty, StudentFemaleFaculty, StudentSexFaculty, StudentInternacionalFaculty, StudentNacionalFaculty, StudentNationalityFaculty, StudentProfessionFaculty, StudentProfessionConstantsFaculty
 
 # metodos
 from etl import etl_process
@@ -40,14 +45,35 @@ sched = BackgroundScheduler(deamon=True)
 sched.add_job(sensor, 'interval', minutes=1)
 sched.start()
 
-#app = Flask(__name__)
+app = Flask(__name__)
+api = Api(app)
+# enable CORS
+CORS(app)
 
 #@app.route("/home")
 #def home():
 #	""" Funcion para fines de prueba. """
 #	return "Bienvenidos :) !"
 
+# cantidad de estudiantes totales
+api.add_resource(Student, '/estudiantes')
+# cantidad de estudiantes masculinos por facultad
+api.add_resource(StudentMaleFaculty, '/estudiantes-masculinos-facultad')
+# cantidad de estudiantes femeninos por facultad
+api.add_resource(StudentFemaleFaculty, '/estudiantes-femeninos-facultad')
+# cantidad de estudiantes por sexo y por facultad
+api.add_resource(StudentSexFaculty, '/estudiantes-sexo-facultad')
+# cantidad de estudiantes internacionales por facultad
+api.add_resource(StudentInternacionalFaculty, '/estudiantes-internacional-facultad')
+# cantidad de estudiantes nacionales por facultad
+api.add_resource(StudentNacionalFaculty, '/estudiantes-nacional-facultad')
+# cantidad de estudiantes por nacionalidad y por facultad
+api.add_resource(StudentNationalityFaculty, '/estudiantes-nacionalidad-facultad')
+# cantidad de estudiantes por carrera y por facultad
+api.add_resource(StudentProfessionFaculty, '/estudiantes-carrera-facultad')
+# cantidad de estudiantes por carrera, dada la facultad
+api.add_resource(StudentProfessionConstantsFaculty, '/estudiantes-carrera/<faculty_code>')
 
 if __name__ == "__main__":
-	main()
-#	app.run()
+	#main()
+	app.run(debug=True)
---
+++ resources/StudentFaculty.py
@@ -0,0 +1,195 @@
+from flask_restful import abort, Resource, reqparse
+import simplejson as json
+from textwrap import dedent
+from cubes import Workspace, Cell, PointCut, Cut
+from flask import make_response
+from pymysql import DatabaseError
+from common.BD import BD
+
+workspace = Workspace()
+workspace.register_default_store("sql", url="mysql+mysqlconnector://root@localhost/prueba")
+workspace.import_model("resources/cubesmodel/model.json")
+browser = workspace.browser("fact_estudiante_facultad")
+
+class Student(Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            
+            r = browser.aggregate()
+            result = {"total-estudiantes": int(r.summary["sumatoria"])}
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentMaleFaculty(BD, Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            params = "Masculino"
+            result = self.queryOne("SELECT * FROM DIM_SEXO WHERE CODIGO = %s", [params])
+            if result is None:
+                abort(404, message="Resource {} doesn't exists".format(params))
+            cut = PointCut("dim_sexo", [result['id']])
+            cell = Cell(browser.cube, cuts = [cut])
+            r = browser.aggregate(cell, drilldown=["dim_sexo", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"sexo": row['dim_sexo.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentFemaleFaculty(BD, Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            params = "Femenino"
+            result = self.queryOne("SELECT * FROM DIM_SEXO WHERE CODIGO = %s", [params])
+            if result is None:
+                abort(404, message="Resource {} doesn't exists".format(params))
+            cut = PointCut("dim_sexo", [result['id']])
+            cell = Cell(browser.cube, cuts = [cut])
+            r = browser.aggregate(cell, drilldown=["dim_sexo", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"sexo": row['dim_sexo.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentSexFaculty(BD, Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            r = browser.aggregate(drilldown=["dim_sexo", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"sexo": row['dim_sexo.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentInternacionalFaculty(BD, Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            params = "Extranjero"
+            result = self.queryOne("SELECT * FROM DIM_NACIONALIDAD WHERE CODIGO = %s", [params])
+            if result is None:
+                abort(404, message="Resource {} doesn't exists".format(params))
+            cut = PointCut("dim_nacionalidad", [result['id']])
+            cell = Cell(browser.cube, cuts = [cut])
+            r = browser.aggregate(cell, drilldown=["dim_nacionalidad", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"nacionalidad": row['dim_nacionalidad.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentNacionalFaculty(BD, Resource):
+    representations = {'application/json': make_response}
+    parser = reqparse.RequestParser()
+    def get(self):
+        try:
+            params = "Venezolano"
+            result = self.queryOne("SELECT * FROM DIM_NACIONALIDAD WHERE CODIGO = %s", [params])
+            if result is None:
+                abort(404, message="Resource {} doesn't exists".format(params))
+            cut = PointCut("dim_nacionalidad", [result['id']])
+            cell = Cell(browser.cube, cuts = [cut])
+            r = browser.aggregate(cell, drilldown=["dim_nacionalidad", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"sexo": row['dim_nacionalidad.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentNationalityFaculty(BD, Resource):
+    def get(self):
+        try:
+            r = browser.aggregate(drilldown=["dim_nacionalidad", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"nacionalidad": row['dim_nacionalidad.codigo'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+class StudentProfessionFaculty(BD, Resource):
+    def get(self):
+        try:
+            r = browser.aggregate(drilldown=["dim_carrera", "dim_facultad"])
+            result = []
+            for row in r:
+                item = {"carrera": row['dim_carrera.nombre'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+
+
+class StudentProfessionConstantsFaculty(BD, Resource):
+    def get(self, faculty_code):
+        try:
+            params = faculty_code
+            result = self.queryOne("SELECT * FROM DIM_FACULTAD WHERE NOMBRE = %s", [params])
+            if result is None:
+                abort(404, message="Resource {} doesn't exists".format(params))
+            cut = PointCut("dim_facultad", [result['id']])
+            cell = Cell(browser.cube, cuts = [cut])
+            r = browser.aggregate(cell, drilldown=["dim_facultad", "dim_carrera"])
+            result = []
+            for row in r:
+                item = {"carrera": row['dim_carrera.nombre'], "facultad": row['dim_facultad.nombre'], "total": row['sumatoria']}
+                result.append(item)
+        except DatabaseError as e:
+            self.rollback()
+            abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+        except Exception as e:
+            abort(500, message="{0}:{1}".format(e.__class__.__name__, e.__str__()))
+
+        return json.dumps(result), 200, { 'Access-Control-Allow-Origin': '*' }
+        
